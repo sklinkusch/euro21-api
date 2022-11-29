57fe0b9a5fa51b26662d2d8ee648e30d28ed65cf
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-11-29 20:25:08 +0100

    respond with World Cup 2022 data

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 3999;
+
+const WM2022 = require("./data/worldcup_m_2022.json")
+
+app.get("/worldcup", (req, res) => getWorldCupData(req, res));
+// app.get("/euro", (req, res) => getEuroData(req, res));
+app.get("*", (req, res) => wrongEndpoint(req, res));
+
+app.listen(port, () => console.log(`Euro21-API listening on port ${port}...`));
+
+function getWorldCupData(req, res) {
+  const { year } = req.query
+  switch (year) {
+    case "2022":
+      return res.status(200).json(WM2022);
+    default:
+      return res.status(500).json({ error: { message: "The data cannot be provided." }});
+  }
+}
