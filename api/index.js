8f4c8112fd7cc71ca33cf0c09db34a9487b9a091
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-12-01 19:34:59 +0100

    change function according to Vercel Docs

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+// const port = 3000;
 
 const WM2022 = require("../data/worldcup_m_2022.json")
 
@@ -17,11 +17,12 @@ app.get("/worldcup", (req, res) => getWorldCupData(req, res));
 // app.get("/euro", (req, res) => getEuroData(req, res));
 app.get("*", (req, res) => wrongEndpoint(req, res));
 
-app.listen(port, () => console.log(`Euro21-API listening on port ${port}...`));
+// app.listen(port, () => console.log(`Euro21-API listening on port ${port}...`));
 
 function getWorldCupData(req, res) {
   const { year } = req.query;
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.setHeader('Content-Type', 'application/json');
   switch (year) {
     case "2022":
