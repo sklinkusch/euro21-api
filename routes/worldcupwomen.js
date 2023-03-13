86910badb089ef85ba8272c823709f818fa6e6dd
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-03-13 21:31:19 +0100

    send data over api

--- routes/worldcupwomen.js
+++ routes/worldcupwomen.js
@@ -1,10 +1,13 @@
 const WM2023 = require("../data/worldcupwomen/worldcup_f_2023");
+const WM2019 = require("../data/worldcupwomen/worldcup_f_2019");
 
 function getWorldCupWomenData(req, res) {
   const { year, lang } = req.query;
   switch (year) {
     case "2023":
       return res.status(200).json(WM2023(lang));
+    case "2019":
+      return res.status(200).json(WM2019(lang));
     default:
       return res
         .status(500)
